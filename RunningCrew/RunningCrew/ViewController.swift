62ae969b486ba9911264cdbcf52d21b0008a1c90
DONOTINTO <goods99j@gmail.com>
2024-04-10 15:37:16 +0900

    create. proj & gitIgnore

---
+++ RunningCrew/RunningCrew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunningCrew
+//
+//  Created by 이중엽 on 4/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
