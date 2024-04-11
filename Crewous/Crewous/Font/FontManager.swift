2264fb18eae351aebabaa3a537738bc81c5d2bf6
DONOTINTO <goods99j@gmail.com>
2024-04-11 18:11:02 +0900

    clean.

--- Crewous/Crewous/Font/FontManager.swift
+++ Crewous/Crewous/Font/FontManager.swift
@@ -29,7 +29,6 @@ enum FontManager {
         case thin = "Thin"
     }
     
-    
     static func getFont(scale: FontScale, size: FontSize) -> UIFont {
         
         let font = UIFont(name: "Blinker-\(scale)", size: size.rawValue) ?? .boldSystemFont(ofSize: size.rawValue)
