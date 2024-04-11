077061af07258019ecdf6e7b8134900593ff7564
DONOTINTO <goods99j@gmail.com>
2024-04-11 18:10:57 +0900

    update. Sign In View

--- Crewous/Crewous/Present/SignIn/SignInView.swift
+++ Crewous/Crewous/Present/SignIn/SignInView.swift
@@ -6,18 +6,59 @@
 //
 
 import UIKit
+import SnapKit
 
 class SignInView: BaseView {
+    
+    let userTextField = UITextField()
+    let passwordTextField = UITextField()
+    let signInButton = UIButton()
+    let createLabel = UILabel()
 
     override func configureHierarchy() {
         
+        [userTextField, passwordTextField, signInButton, createLabel].forEach { addSubview($0) }
     }
 
     override func configureLayout() {
         
+        userTextField.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(20)
+            $0.bottom.equalTo(self.snp.centerY)
+            $0.height.equalTo(50)
+        }
+        
+        passwordTextField.snp.makeConstraints {
+            $0.top.equalTo(userTextField.snp.bottom).offset(30)
+            $0.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(50)
+        }
+        
+        signInButton.snp.makeConstraints {
+            $0.top.greaterThanOrEqualTo(passwordTextField.snp.bottom).offset(50)
+            $0.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(50)
+        }
+        
+        createLabel.snp.makeConstraints {
+            $0.top.greaterThanOrEqualTo(signInButton.snp.bottom).offset(100)
+            $0.centerX.equalTo(self)
+            $0.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom).inset(30)
+        }
     }
     
     override func configureView() {
         
+        userTextField.custom(placeholder: "User Email", imageStr: "person.fill")
+        userTextField.keyboardType = .emailAddress
+        
+        passwordTextField.custom(placeholder: "User Eamil", imageStr: "lock.fill")
+        
+        signInButton.custom(title: "Sign In", titleColor: .black, bgColor: .customGreen)
+        
+        createLabel.text = "Create Account"
+        createLabel.textColor = .customGray
+        createLabel.font = FontManager.getFont(scale: .bold, size: .small)
+        createLabel.isUserInteractionEnabled = true
     }
 }
--- Crewous/Crewous/Present/SignIn/SignInViewController.swift
+++ Crewous/Crewous/Present/SignIn/SignInViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class SignInViewController: BaseViewController<SignInView> {
 
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func bind() {
         
+        let tapGesture = UITapGestureRecognizer()
+        layoutView.createLabel.addGestureRecognizer(tapGesture)
+        
+        // SignUP VC로 이동
+        tapGesture.rx.event.bind(with: self) { owner, _ in
+            
+        }.disposed(by: disposeBag)
     }
 }
 
