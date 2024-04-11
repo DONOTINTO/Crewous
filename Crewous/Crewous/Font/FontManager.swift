855abdf3ea80d2d0a64684e3f480fc525a8ea80c
DONOTINTO <goods99j@gmail.com>
2024-04-11 15:57:25 +0900

    update. Font Manager

---
+++ Crewous/Crewous/Font/FontManager.swift
@@ -0,0 +1,39 @@
+//
+//  FontManager.swift
+//  Crewous
+//
+//  Created by 이중엽 on 4/11/24.
+//
+
+import UIKit.UIFont
+
+enum FontManager {
+    
+    enum FontSize: CGFloat {
+        
+        case small = 15
+        case medium = 20
+        case large = 30
+    }
+    
+    enum FontScale: String {
+        
+        case black = "Black"
+        case bold = "Bold"
+        case extraBold = "ExtraBold"
+        case headLine = "Headline"
+        case light = "Light"
+        case regular = "Regular"
+        case semiBold = "SemiBold"
+        case semiLight = "SemiLight"
+        case thin = "Thin"
+    }
+    
+    
+    static func getFont(scale: FontScale, size: FontSize) -> UIFont {
+        
+        let font = UIFont(name: "Blinker-\(scale)", size: size.rawValue) ?? .boldSystemFont(ofSize: size.rawValue)
+        
+        return font
+    }
+}
